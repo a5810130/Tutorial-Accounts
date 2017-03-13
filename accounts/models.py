f6da2ee135e50e456bb0dd8d1c33d9aad1a3b62d
a5810130 <a5810130@gmail.com>
2017-03-13 15:18:40 +0700

    2.create model

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+
+class Account(models.Model):
+    name = models.CharField(max_length=30)
+    create = models.DateTimeField('created')
+    
+    def __str__(self):
+        return self.name
+
+
+class Transaction(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    date = models.DateField('date')
+    actionType = models.CharField(max_length=10) 
+    description = models.CharField(max_length=200)
+    value = models.DecimalField(max_digits=14, decimal_places=2)
+    
+    def __str__(self):
+        return self.description
