eaf7fb31e47dc9bf656037a318f4d4cbaebba02b
a5810130 <a5810130@gmail.com>
2017-03-15 18:26:21 +0700

    5. form

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,6 @@ app_name = 'accounts'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<account_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^add_account/$', views.add_account, name='add_account'),
+    url(r'^(?P<account_id>[0-9]+)/add_transaction/$', views.add_transaction, name='add_transaction'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.utils import timezone
+from django.urls import reverse
+from datetime import datetime
 
 from .models import Account, Transaction
 
@@ -13,3 +16,34 @@ def detail(request, account_id):
     transaction = account.transaction_set.order_by('-date','-id')
     context = {'account':account, 'transaction_list':transaction}
     return render(request, 'accounts/detail.html', context)
+
+def add_account(request):
+    try:
+        account_name = request.POST['account_name']
+        if not account_name.strip():
+            raise 
+    except :
+        return HttpResponseRedirect(reverse('accounts:index',))
+    else:
+        a = Account(name=account_name, create=timezone.now())
+        a.save()
+        return HttpResponseRedirect(reverse('accounts:index',))
+    
+def add_transaction(request, account_id):
+    account = get_object_or_404(Account, pk=account_id)
+    try:
+        date_str = request.POST['date']
+        description = request.POST['description']
+        actionType = request.POST['type']
+        value = request.POST['value']
+        if (not date_str.strip() or not description.strip()
+        or not value.strip()):
+            raise 
+    except :
+        return HttpResponseRedirect(reverse('accounts:detail', args=(account_id)))
+    date = datetime.strptime(date_str,"%m/%d/%Y")
+    account.transaction_set.create( date=date, actionType=actionType,
+                            description=description,
+                            value=value)
+    account.save()
+    return HttpResponseRedirect(reverse('accounts:detail', args=(account_id)))
